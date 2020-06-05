3c37faf10537c20218fb29de16c5b991e0699717
amrha <48659223+amrha@users.noreply.github.com>
2020-06-05 16:51:54 +0100

    Add files via upload

---
+++ get_news/main.py
@@ -0,0 +1,19 @@
+from flask import Flask,jsonify
+from flask import Blueprint
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+app=Flask(__name__)
+CORS(app)
+cors=CORS(app,resources={r"/*":{"origins":"*"}})
+app.config["MONGO_URI"]="mongodb+srv://amrou:23856415@cluster0-k8dcj.gcp.mongodb.net/test?retryWrites=true&w=majority"
+app.config['MONGO_DBNAME'] = 'news'
+mongo=PyMongo(app)
+@app.route('/')
+def index():
+	news = mongo.db.news 
+	result = []
+	for field in news.find():
+		result.append({'title':field['title'],'content':field['content'],'time':field['time'],'by':field['by'],'url':field['url']})
+	return jsonify(result)
+if __name__=='__main__':
+	app.run(debug=True)
